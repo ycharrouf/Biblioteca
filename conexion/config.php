1de31dd031ec71f283bfe438d1998413facf9ebd
ycharrouf <ycha0707@g.educaand.es>
2024-12-03 18:17:41 +0100

    Practia 2 hecha

--- conexion/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_SERVER', '172.21.0.2');
-define('DB_USERNAME', 'biblioteca');
-define('DB_PASSWORD', '1234');
-define('DB_DATABASE', 'pruebaBiblioteca');
-define('DB_PORT', 3306);
-define('DB_PREFIX', ''); 
-define('DB_DRIVER', 'mysql');
